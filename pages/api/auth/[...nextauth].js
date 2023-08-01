70a2f7ae2abe181d977be3d4447707503393accf
alexissfry <alexissfry@gmail.com>
2023-07-31 18:44:52 -0700

    feat: trying to make request

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,14 +1,41 @@
-import NextAuth from 'next-auth';
-import GoogleProvider from 'next-auth/providers/google';
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
 export const authOptions = {
- providers: [
-  GoogleProvider({
-   clientId: process.env.GOOGLE_ID,
-   clientSecret: process.env.GOOGLE_SECRET,
-  }),
- ],
- session: {
-  strategy: 'jwt',
- },
+  secret: "WORIHNTWORGNWIRGWRGIWRGIWRGIWsisnfgoar",
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+      scope: ["https://mail.google.com/"],
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
+    }),
+  ],
+  refreshToken: true,
+  session: {
+    strategy: "jwt",
+  },
+  callbacks: {
+    async session({ session, token, user }) {
+      session.accessToken = token.accessToken;
+      session.user.id = token.id;
+
+      return session;
+    },
+    async jwt({ token, account, profile }) {
+      if (account) {
+        token.accessToken = account.access_token;
+        token.id = profile.id;
+      }
+      return token;
+    },
+  },
 };
+
 export default NextAuth(authOptions);
--- pages/emails.js
+++ pages/emails.js
@@ -3,14 +3,39 @@ import { useEmail } from "./EmailContext";
 import { useSession } from "next-auth/react";
 
 export default function Emails() {
-    const { emailNum } = useEmail();
-    console.log("type of emailnum", typeof emailNum);
-    const { data: session, status } = useSession();
-    const { accessToken } = session;
-    console.log("session", session);
-    return (
-        <div>
-            <h1>{emailNum}</h1>
-        </div>
-    );
+  const { emailNum } = useEmail();
+  const { data: session, status } = useSession();
+  const { accessToken } = session;
+
+  async function getMessages() {
+    const axios = require("axios");
+    console.log(session);
+    console.log(accessToken);
+
+    try {
+      const response = await axios.get(
+        `https://gmail.googleapis.com/gmail/v1/users/${encodeURIComponent(
+          session?.user?.email
+        )}/messages`,
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+          params: {
+            maxResults: emailNum,
+            q: "is:unread",
+          },
+        }
+      );
+      console.log(response.data.messages);
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+  getMessages();
+  return (
+    <div>
+      <h1>{emailNum}</h1>
+    </div>
+  );
 }
